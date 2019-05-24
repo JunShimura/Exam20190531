f9fcb05c611a59e7776c8344aa5125d4e432d003
junshimura <junshimura@gmail.com>
2019-05-24 17:18:48 +0900

    Commented

--- Assets/Script/DetermineNumber.cs
+++ Assets/Script/DetermineNumber.cs
@@ -6,12 +6,18 @@ using UnityEngine.SceneManagement;
 
 public class DetermineNumber : MonoBehaviour
 {
+    #region Set in Editor
     [SerializeField] Dropdown dropdown;
     [SerializeField] Text answerText;
     [SerializeField] NumValueManager numValueManager1;
     [SerializeField] NumValueManager numValueManager2;
+    #endregion
+    
+    #region DelegateTable
+    // Dalegate list for the selected procedure
     delegate string DetermineNumberQuiz(int n1, int n2);
     List<DetermineNumberQuiz> determineNumberQuiz = new List<DetermineNumberQuiz>();
+    #endregion
 
     // Random 
     int r1;
@@ -21,7 +27,6 @@ public class DetermineNumber : MonoBehaviour
     //　回数
     int answerdTimes = 0;
 
-    //
     private void Awake()
     {
         determineNumberQuiz.Add(Quiz0);
@@ -56,6 +61,14 @@ public class DetermineNumber : MonoBehaviour
         }
 
     }
+
+    /// <summary>
+    /// 一つの答えを出すサンプル
+    /// 入力値1にある値とランダムを比較、当たりか判定
+    /// </summary>
+    /// <param name="n1">入力値1</param>
+    /// <param name="n2">入力値2</param>
+    /// <returns>判定結果</returns>
     string Quiz0(int n1, int n2)
     {
         answerdTimes++;
@@ -70,11 +83,44 @@ public class DetermineNumber : MonoBehaviour
         }
         return answer;
     }
+
+    /// <summary>
+    /// 1から10の数をPCが設定し（乱数の使い方を調べて使用する）
+    ///    その数を入力して当てる。入力回数を表示する。
+    /// メッセージは、
+    /// ・答えとの差の絶対値が3以下の場合：「近い」
+    /// ・答えとの差の絶対値が1の場合：「惜しい」
+    /// ・正解の場合：「当たり！」
+    /// ・上記のいずれにも該当しない場合：「ハズレ」
+    /// 例）答えが3の場合
+    /// 入力が8:「1回目：ハズレ」
+    /// 入力が5:「2回目：近い」
+    /// 入力が2:「3回目：惜しい」
+    /// 入力が3:「4回目：当たり！」
+    /// </summary>
+    /// <param name="n1">入力値1</param>
+    /// <param name="n2">入力値2</param>
+    /// <returns>判定結果</returns>
     string Quiz1(int n1, int n2)
     {
         string answer = "まだやってません1";
         return answer;
     }
+
+
+    /// <summary>
+    /// (2)2つの数あてゲーム
+    /// 1から10の数を2種類、PCが設定し（乱数の使い方を調べて使用する）
+    /// その数を入力して当てる。入力回数を表示する。
+    /// メッセージは、それぞれの数の状態によって分かれる
+    /// ・答えとの差の絶対値が3以下の場合：「片方近い」「両方近い」
+    /// ・答えとの差の絶対値が1の場合：「片方惜しい」「両方惜しい」
+    /// ・正解の場合：「片方当たり！」「両方当たり！」
+    /// ・上記のいずれにも該当しない場合：「ハズレ」
+    /// </summary>
+    /// <param name="n1">入力値1</param>
+    /// <param name="n2">入力値2</param>
+    /// <returns>判定結果</returns>
     string Quiz2(int n1, int n2)
     {
         string answer = "まだやってません2";
